53fde930fd7bd5487e45ee927a637c7f6a813cb4
mariano.palliser <mariano.palliser@gmail.com>
2023-09-10 12:04:38 +0200

    standalone app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActionsComponent } from './components/actions/actions.component';
+import { TableComponent } from './components/table/table.component';
 
 @Component({
+  standalone: true,
+  imports: [
+    CommonModule,
+    ActionsComponent,
+    TableComponent,
+  ],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  template: `
+  <app-actions></app-actions>
+  
+  <app-table></app-table>`,
 })
-export class AppComponent {
-  title = 'megasport-app';
-}
+export class AppComponent {}
