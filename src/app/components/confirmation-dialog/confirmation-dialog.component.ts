bdd4b7e6811b9060ae5e4557151ad37a6fe4fcb8
mariano.palliser <mariano.palliser@gmail.com>
2023-09-23 19:03:33 +0200

    review confirmation styles

--- src/app/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common'
-import { Component, Inject, ViewEncapsulation } from '@angular/core'
+import { Component, Inject } from '@angular/core'
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog'
@@ -20,7 +20,6 @@ import { EventDto } from 'src/app/models/event-dto'
   selector: 'app-confirmation-dialog',
   templateUrl: './confirmation-dialog.component.html',
   styleUrls: ['./confirmation-dialog.component.sass'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class ConfirmationDialogComponent {
   public formControl = new FormControl<string>('', [Validators.required, Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)])
