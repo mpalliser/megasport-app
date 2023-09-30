90989cac4f3733c33d1a0cf70ac8465af5c6dd46
mariano.palliser <mariano.palliser@gmail.com>
2023-09-30 20:20:44 +0200

    add locale

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { DatePipe } from '@angular/common'
+import { DatePipe, registerLocaleData } from '@angular/common'
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
-import { importProvidersFrom } from '@angular/core'
+import { LOCALE_ID, importProvidersFrom } from '@angular/core'
 import { bootstrapApplication } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { Routes, provideRouter } from '@angular/router'
 import { throwError } from 'rxjs'
-import { LoaderInterceptor } from './app/interceptors/loader.interceptor'
+import localeEs from '@angular/common/locales/es'
 import { AppComponent } from './app/app.component'
+import { LoaderInterceptor } from './app/interceptors/loader.interceptor'
 
 const routes: Routes = [
   {
@@ -15,12 +16,15 @@ const routes: Routes = [
   },
 ]
 
+registerLocaleData(localeEs, 'es')
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
     importProvidersFrom(BrowserAnimationsModule),
     DatePipe,
+    { provide: LOCALE_ID, useValue: 'es' },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: LoaderInterceptor,
